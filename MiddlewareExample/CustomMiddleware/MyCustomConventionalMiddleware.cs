85cc55c1dd3c97ec0e02e86564485ad9daadbf92
Can Demirbüken <yasarcandemirbuken4@outlook.com>
2024-12-15 16:30:27 +0300

    Added HTTP and Middleware folders to AspNetCoreTutorials repository

---
+++ MiddlewareExample/CustomMiddleware/MyCustomConventionalMiddleware.cs
@@ -0,0 +1,31 @@
+﻿namespace MiddlewareExample.CustomMiddleware
+{
+    public class MyCustomConventionalMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public MyCustomConventionalMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Query.ContainsKey("firstname") && context.Request.Query.ContainsKey("lastname"))
+            {
+                string fullName = context.Request.Query["firstname"] + " " + context.Request.Query["lastname"];
+                await context.Response.WriteAsync(fullName + "\n");
+            }
+
+            await _next(context);
+        }
+    }
+
+    public static class CustomConventionalMiddlewareExtension
+    {
+        public static IApplicationBuilder UseCustomConventionalMiddlewareExtension(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<MyCustomConventionalMiddleware>();
+        }
+    }
+}
