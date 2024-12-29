85c3c46f8de3a24084b309b2a38de12b25c46f93
Can <103042124+CanDemirbuken@users.noreply.github.com>
2024-12-29 19:39:14 +0300

    E-Commerce Order App added.

---
+++ Model Binding and Validations/18. E-Commerce Orders App/18. E-Commerce Orders App/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using _18._E_Commerce_Orders_App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _18._E_Commerce_Orders_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/order")]
+        public IActionResult Index([Bind(nameof(Order.OrderDate), nameof(Order.InvoicePrice), nameof(Order.Products))] Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                string messages = string.Join("\n", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage));
+                return BadRequest(messages);
+            }
+
+            Random random = new Random();
+            int randomOrderNumber = random.Next(1, 99999);
+
+            return Json(new { orderNumber = randomOrderNumber });
+        }
+    }
+}
