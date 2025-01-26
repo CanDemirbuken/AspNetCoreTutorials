37744b0ca202504731cec49b41964a7c3e488b4d
Can <103042124+CanDemirbuken@users.noreply.github.com>
2025-01-26 18:47:55 +0300

    Delete ServiceContracts directory

--- ServiceContracts/IFinhubService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ServiceContracts
-{
-    public interface IFinhubService
-    {
-        Dictionary<string, object> GetCompanyProfile(string stockSymbol);
-        Dictionary<string, object> GetStockPriceQuote(string stockSymbol);
-    }
-}
