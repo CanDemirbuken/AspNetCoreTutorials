c4fbbf8010044274dd96d84ae9b8bcccb6cceb85
Can <103042124+CanDemirbuken@users.noreply.github.com>
2025-01-26 18:59:30 +0300

    Delete xUnit/Controllers & IActionResult directory

--- xUnit/Controllers & IActionResult/09. Bank App Tutorial/09. Bank App Tutorial/Models/AccountDetails.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _09._Bank_App_Tutorial.Models
-{
-    public class AccountDetails
-    {
-        public int AccountNumber { get; set; }
-        public string? AccountHolderName { get; set; }
-        public decimal CurrentBalance { get; set; }
-    }
-}
