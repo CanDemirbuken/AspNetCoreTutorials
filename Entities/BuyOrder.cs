64c4248a535fe5ea759e4e371e71708b127c57f1
Can Demirbüken <yasarcandemirbuken4@outlook.com>
2025-01-26 18:46:36 +0300

    27. Stock App with xUnit added

---
+++ Entities/BuyOrder.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class BuyOrder
+    {
+        public Guid BuyOrderID { get; set; }
+
+        [Required(ErrorMessage = "Stock Symbol is required.")]
+        public string StockSymbol { get; set; }
+
+        [Required(ErrorMessage = "Stock Name is required.")]
+        public string StockName { get; set; }
+        public DateTime DateAndTimeOfOrder { get; set; }
+
+        [Range(1, 100000, ErrorMessage = "Quantity should be between 1 and 100000")]
+        public uint Quantity { get; set; }
+
+        [Range(1.00, 100000.00, ErrorMessage = "Price should be between 1 and 100000")]
+        public double Price { get; set; }
+    }
+}
