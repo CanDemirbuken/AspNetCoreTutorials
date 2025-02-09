e7b9722d1cdf313a10099ec99ee5eff949b664cb
Can <103042124+CanDemirbuken@users.noreply.github.com>
2025-02-09 17:48:28 +0300

    Entity Framework Core section added.

---
+++ Entity Framework/Layout Views/09. Weather App with Layout/09. Weather App with Layout/Models/CityWeather.cs
@@ -0,0 +1,10 @@
+﻿namespace _09._Weather_App_with_Layout.Models
+{
+    public class CityWeather
+    {
+        public string CityUniqueCode { get; set; }
+        public string CityName { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public int TemperatureFahrenheit { get; set; }
+    }
+}
